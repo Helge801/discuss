a9b83f8553645eaf10635345e0c46a65edd352a5
Bryan Lund <bryan.helge@gmail.com>
2019-06-09 16:07:18 -0600

    initial handling of Topic vreat

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,13 +3,18 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do
-    changeset = Topic.changeset(%Topic{}, params)
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"topic" => topic}) do
-    IO.expect(topic)
+  def create(_conn, %{"topic" => topic}) do
+    Topic.changeset(%Topic{}, topic)
+    |> Repo.insert
+    |> case do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
   end
 
 end
